df44a820d4bcace961854053804231def07dba44
Arafa Dev <arafa.dev@gmail.com>
2024-07-11 21:33:28 +0300

    realtime firist commit

---
+++ routes/web.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminHomeController;
+use App\Http\Controllers\AdminProfileController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
+
+/*
+|--------------------------------------------------------------------------
+| Admin Routes
+|--------------------------------------------------------------------------
+|
+*/
+Route::prefix('admin')->name('admin.')->group(function () {
+
+    Route::middleware(['admin'])->group(function () {
+
+        ##------------------------------------------------------- ADMIN INDEX PAGE
+        Route::get('/', AdminHomeController::class)->name('index');
+
+        Route::get('/notification/markasread', function () {
+
+            Auth::guard('admin')->user()->notifications->markAsRead();
+
+
+        })->name('notifications.read');
+        
+        ##------------------------------------------------------- CLEAR ALL NOTIFICATIONS
+        Route::get('/notification/clear', function () {
+            Auth::guard('admin')->user()->notifications()->delete();
+        })->name('notifications.clear');
+
+    });
+
+    require __DIR__ . '/adminAuth.php';
+});
